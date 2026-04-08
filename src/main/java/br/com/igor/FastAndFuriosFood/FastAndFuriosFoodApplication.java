815707888fc7d4cff7732cf73663e5974cc18318
Igor <igorcrispim154@gmail.com>
2026-04-08 10:03:36 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/igor/FastAndFuriosFood/FastAndFuriosFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.igor.FastAndFuriosFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastAndFuriosFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastAndFuriosFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/igor/FastAndFuriosFood/FastAndFuriosFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igor.FastAndFuriosFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastAndFuriosFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
